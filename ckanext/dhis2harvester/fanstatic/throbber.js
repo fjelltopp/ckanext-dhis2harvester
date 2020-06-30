513e4e8276f0fe4c637cae49a9b5c41a871f8ac7
Tomasz Sabała <sabala.tomek@gmail.com>
2020-06-30 09:04:33 +0200

    ADX-318 Codestyle changes.

--- ckanext/dhis2harvester/fanstatic/throbber.js
+++ ckanext/dhis2harvester/fanstatic/throbber.js
@@ -1,6 +1,3 @@
-// Enable JavaScript's strict mode. Strict mode catches some common
-// programming errors and throws exceptions, prevents some unsafe actions from
-// being taken, and disables some confusing and bad JavaScript features.
 "use strict";
 
 this.ckan.module('throbber', function ($) {
