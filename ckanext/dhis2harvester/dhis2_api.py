01f987ddc1e653f26746897c608a8af937cd77c8
Tomasz Sabała <tomek@fjelltopp.org>
2022-12-06 15:30:33 +0100

    ADX-912 Fixup multiple dhis2 api version.

--- ckanext/dhis2harvester/dhis2_api.py
+++ ckanext/dhis2harvester/dhis2_api.py
@@ -13,24 +13,7 @@ from ckanext.dhis2harvester import request_util
 log = logging.getLogger(__name__)
 
 API_CONFIG = {
-    26: {
-        "PIVOT_TABLES_RESOURCE": "reportTables.json?"
-                                 "fields=id,displayName~rename(name),created,lastUpdated,access,title,description,user&"
-                                 "order=name:asc&paging=false",
-        "PIVOT_TABLES_KEY_NAME": "reportTables",
-        "PIVOT_TABLES_CSV_RESOURCE": 'analytics.csv?'
-                                     'dimension=dx:{data_elements}&'
-                                     'dimension=pe:{periods}&'
-                                     'dimension=co&'
-                                     'dimension=ou:{organisation_units}&'
-                                     'displayProperty=NAME&'
-                                     'hierarchyMeta=true&'
-                                     'outputIdScheme=UID',
-        "PIVOT_TABLE_KEYS": ["lastUpdated", "created", "id", "name"],
-        "SECURITY_LOGIN_ACTION": 'dhis-web-commons-security/login.action',
-        "ORG_UNIT_RESOURCE": "organisationUnits?paging=false&fields=id,name"
-    },
-    28: {
+    37: {
         "PIVOT_TABLES_RESOURCE": 'visualizations.json?type=PIVOT_TABLE&'
                                  'fields=id,displayName~rename(name),created,lastUpdated,access,title,description,user&'
                                  'order=name:asc&paging=false',
@@ -48,14 +31,29 @@ API_CONFIG = {
         "ORG_UNIT_RESOURCE": "organisationUnits?paging=false&fields=id,name"
     }
 }
+# Default DHIS2 api configuration working till /api/36
+DEFAULT_API_CONFIG = {
+    "PIVOT_TABLES_RESOURCE": "reportTables.json?"
+                             "fields=id,displayName~rename(name),created,lastUpdated,access,title,description,user&"
+                             "order=name:asc&paging=false",
+    "PIVOT_TABLES_KEY_NAME": "reportTables",
+    "PIVOT_TABLES_CSV_RESOURCE": 'analytics.csv?'
+                                 'dimension=dx:{data_elements}&'
+                                 'dimension=pe:{periods}&'
+                                 'dimension=co&'
+                                 'dimension=ou:{organisation_units}&'
+                                 'displayProperty=NAME&'
+                                 'hierarchyMeta=true&'
+                                 'outputIdScheme=UID',
+    "PIVOT_TABLE_KEYS": ["lastUpdated", "created", "id", "name"],
+    "SECURITY_LOGIN_ACTION": 'dhis-web-commons-security/login.action',
+    "ORG_UNIT_RESOURCE": "organisationUnits?paging=false&fields=id,name"
+}
 
 
 class Dhis2Connection(object):
-    DEFAULT_API_VERSION = '26'
 
     def __init__(self, url, username=None, password=None, auth_token=None, api_version=None):
-        if not api_version:
-            api_version = self.DEFAULT_API_VERSION
         self.url = self.__add_trailing_slash(url)
         self.api_version = api_version
         self.api_url = self.__api_url(self.url, self.api_version)
@@ -91,11 +89,12 @@ class Dhis2Connection(object):
         return headers
 
     def __setup_api_config(self):
-        config = {}
         for version in reversed(sorted(API_CONFIG.keys())):
             if version <= int(self.api_version):
                 config = API_CONFIG[version]
                 break
+        else:
+            config = DEFAULT_API_CONFIG
         for key, value in config.items():
             setattr(self, key, value)
 
